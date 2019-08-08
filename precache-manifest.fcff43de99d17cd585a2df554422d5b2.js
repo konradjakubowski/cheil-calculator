97105d77ab16ef0f904e98b7fdda146faa167189
Konrad Jakubowski <jakubowski.info@gmail.com>
2019-08-08 19:54:49 +0200

    Updates

---
+++ precache-manifest.fcff43de99d17cd585a2df554422d5b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1549b8d2c804fb82be19b183869266",
+    "url": "/cheil-calculator/index.html"
+  },
+  {
+    "revision": "434cdddd42a195091317",
+    "url": "/cheil-calculator/static/css/main.238c2d68.chunk.css"
+  },
+  {
+    "revision": "0143a485c2dc87e8534b",
+    "url": "/cheil-calculator/static/js/2.4a6d9eed.chunk.js"
+  },
+  {
+    "revision": "434cdddd42a195091317",
+    "url": "/cheil-calculator/static/js/main.24c32945.chunk.js"
+  },
+  {
+    "revision": "5b55969d09e9f224d037",
+    "url": "/cheil-calculator/static/js/runtime~main.34b1a255.js"
+  },
+  {
+    "revision": "8036464060fe818a6f5e01ed0e97a574",
+    "url": "/cheil-calculator/static/media/SamsungSharpSans-Bold.80364640.ttf"
+  },
+  {
+    "revision": "63bce755b8e968296e1a048939056364",
+    "url": "/cheil-calculator/static/media/SamsungSharpSans-Medium.63bce755.ttf"
+  },
+  {
+    "revision": "d09b9b547d7043e57cc5b764afb0db6b",
+    "url": "/cheil-calculator/static/media/SamsungSharpSans-Regular.d09b9b54.ttf"
+  },
+  {
+    "revision": "1598acd3254d6883293ed8f8241b0a78",
+    "url": "/cheil-calculator/static/media/calc-background.1598acd3.png"
+  }
+]);
